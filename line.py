8190f018b277787e1804210b29b18b94387566c4
Nick Tammadge <nicktammadge@gmail.com>
2023-11-29 11:45:51 -0800

    Merge types into one file because they're rarely used separately

--- line.py
+++ line.py
@@ -1,9 +1,13 @@
-from point import Point
 import string
 from tkinter import Canvas
 
+class Point:
+    def __init__(self, x: float, y: float) -> None:
+        self.x = x
+        self.y = y
+
 class Line:
-    def __init__(self, point1: Point, point2: Point):
+    def __init__(self, point1: Point, point2: Point) -> None:
         self.__point1 = point1
         self.__point2 = point2
 
--- point.py
+++
@@ -1,4 +0,0 @@
-class Point:
-    def __init__(self, x: float, y: float):
-        self.x = x
-        self.y = y
