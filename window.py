54d75bcb846b3010296fc6a16d976cf83dfa2366
Nick Tammadge <nicktammadge@gmail.com>
2023-11-29 11:46:20 -0800

    Re-add default return info from snippet

--- window.py
+++ window.py
@@ -3,7 +3,7 @@ import string
 from tkinter import Tk, BOTH, Canvas
 
 class Window:
-    def __init__(self, width, height):
+    def __init__(self, width, height) -> None:
         self.__root = Tk()
         self.__root.title = "Test window"
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
